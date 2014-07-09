e0ac5c0122e0985fefb0b0c9b4dbb3ebb8f9bc72
Ryan Alyn Porter <rap@endymion.com>
2014-07-09 17:20:06 -0400

    Added a method for generating an API access URL.
    Uses the signature generator.

--- lib/gravity_forms_api.rb
+++ lib/gravity_forms_api.rb
@@ -4,6 +4,17 @@ require 'cgi'
 
 class GravityFormsAPI
 
+  def self.generate_URL site:nil, route:nil, public_api_key:nil, private_api_key:nil, http_method:'GET'
+    expires = (Time.now + 60*60).to_i # One hour.
+    "http://#{site}/gravityformsapi/#{route}?api_key=#{public_api_key}&expires=#{expires}&" +
+      "signature=" + generate_signature(
+        api_key:public_api_key,
+        private_key:private_api_key,
+        http_method:http_method,
+        route:route,
+        expires:expires)
+  end
+
   def self.generate_signature api_key:nil, expires:nil, http_method:nil, route:nil, private_key:nil
     CGI::escape(
       Base64.encode64(
